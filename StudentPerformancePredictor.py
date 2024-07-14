e9a4c16234c2022d497d4487b1ff31899bdc2e29
zyle-geralde <zylgerdlp19@gmail.com>
2024-07-15 02:05:18 +0800

    finished product console only

--- StudentPerformancePredictor.py
+++ StudentPerformancePredictor.py
@@ -15,14 +15,47 @@ def load_data():
     one_hot = OneHotEncoder();
     transformer = ColumnTransformer([("one_hot",one_hot,categoical_feature,)],remainder="passthrough")
     transformedx = transformer.fit_transform(data)
-    print(transformedx.shape)
     np_array = data.values # convert pandas dataframe to numpy array
     x = transformedx[:,:6]
     y = transformedx[:,6]
     return x,y,np_array
 
 
-x_train,y_train,np = load_data();
+x_train,y_train,np_arr = load_data();
+
+scale = StandardScaler();
+x_norm = scale.fit_transform(x_train)
+#print(x_norm)
+
+# Display peak-to-peak ranges
+#print("Peak to Peak range by column in x_train:")
+#print(np.ptp(x_train, axis=0))  # Use np.ptp(arr, axis=0) to compute peak-to-peak range
+#print("Peak to Peak range by column in x_norm:")
+#print(np.ptp(x_norm, axis=0))   # Use np.ptp(arr, axis=0) to compute peak-to-peak range
+
+sgdr = SGDRegressor(max_iter=1000000)
+sgdr.fit(x_norm,y_train)
+
+w_norm = sgdr.coef_
+b_norm = sgdr.intercept_
+
+#print(f"w:{w_norm},b:{b_norm}")
+y_pred = sgdr.predict(x_norm)
+#print(f"Predicted Value:{y_pred}\nActual Value: {y_train}")
+
+fig,ax = plt.subplots(2,3,figsize = (12,6),sharey=True);
+
+'''countme = 0;
+for i in range(2):
+    for j in range(3):
+        ax[i,j].scatter(x_train[:,countme],y_train)
+        ax[i,j].scatter(x_train[:,countme],y_pred)
+        countme+=1
+
+plt.show();'''
+
+
+
 
 
 
